ffe1d6fe2faf4b25ea5e84a9d26f32a7de8abeee
Entrar <Entrar@APD-L13>
2017-10-27 12:07:14 +0100

    Primera Versión de Kata4

---
+++ src/kata4/view/MailListReader.java
@@ -0,0 +1,26 @@
+package kata4.view;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MailListReader {
+    public static List<String> read (String filename) throws FileNotFoundException, IOException{
+        List<String> mailList = new ArrayList();
+        BufferedReader reader = new BufferedReader(new FileReader(new File(filename)));
+        String mail;
+        while ((mail = reader.readLine()) != null){
+            if (mail.contains("@")){
+                mailList.add(mail);
+            }
+        }
+        //Hay que poner los try 
+        reader.close();
+        return mailList;
+    }
+
+}
