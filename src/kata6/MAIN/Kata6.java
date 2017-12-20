b9f1ac6f5e46be20e32bace44c5bb62dcdf6464a
Experimentos <Experimentos@Experimentos-PC>
2017-12-20 10:42:35 +0000

    Kata 6 Tercera Versión (Final)

--- src/kata6/MAIN/Kata6.java
+++ src/kata6/MAIN/Kata6.java
@@ -1,9 +1,12 @@
 package kata6.MAIN;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 import kata6.model.Histogram;
 import kata6.model.Mail;
+import kata6.model.Person;
+import kata6.view.DataBaseList;
 import kata6.view.HistogramDisplay;
 import kata6.view.HistogramBuilder;
 import kata6.view.MailListReader;
@@ -14,21 +17,26 @@ public class Kata6 {
     private HistogramBuilder<Mail> builder;
     private Histogram<String> domains;
     private Histogram<Character> letters;
+    private List<Person> people;
+    private HistogramBuilder<Person> builderPerson;
+    private Histogram<Character> gender;
+    private Histogram<Float> weight;
     
-    public static void main(String[] args) throws IOException {     
+    public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {     
         Kata6 kata = new Kata6();
         kata.execute(); 
     }
     
-    private void execute() throws IOException{
+    private void execute() throws IOException, ClassNotFoundException, SQLException{
         input();
         process();
         output();
     }
     
-    private void input() throws IOException{
+    private void input() throws IOException, ClassNotFoundException, SQLException{
         String filename = "emails.txt";
         mailList = MailListReader.read(filename);
+        people = DataBaseList.read();
     }
     
     private void process(){
@@ -45,16 +53,25 @@ public class Kata6 {
                 return item.getMail().charAt(0);
             }
         });
-        
+        builderPerson = new HistogramBuilder<>(people);
+        gender = builderPerson.build(new Attribute <Person, Character>(){
+            @Override
+            public Character get(Person item){
+                return item.getGender();
+            }
+        });
+        weight = builderPerson.build(new Attribute <Person, Float>(){
+            @Override
+            public Float get(Person item){
+                return item.getWeight();
+            }
+        });        
     }
     
     private void output(){
-        /*HistogramDisplay histoDisplay = new HistogramDisplay(domains, "Dominios");
-        histoDisplay.execute();
-        histoDisplay = new HistogramDisplay(letters, "Primer caracter");
-        histoDisplay.execute();*/
         new HistogramDisplay(domains, "Dominios").execute();
         new HistogramDisplay(letters, "Primer caracter").execute();
+        new HistogramDisplay(gender, "Gender").execute();
+        new HistogramDisplay(weight, "Weight").execute();
     }    
 }
-
